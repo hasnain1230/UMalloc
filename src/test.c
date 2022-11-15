bcdebe10dbd3451bc743e6e9e7a63084efdfe354
Hasnain <hasnain1230@gmail.com>
2022-11-15 16:35:34 -0500

    Fixed the meta data being too big bug

--- src/test.c
+++ src/test.c
@@ -6,7 +6,56 @@
 
 #include "umalloc.h"
 
+void unusedFunction(char **arr) { // This is done to supress unused variable error provided in the -Wextra flag that we are using.
+    arr = arr;
+    return;
+}
 
-int main(void) {
+int main() {
+    for (int i = 0; i < 50; i++) {
+        int *array = (int *) malloc (120 * sizeof(int)); // Make Array of size 120.
 
+        if (array == NULL) {
+            perror("Malloc Returned NULL. Please see error message above. Program is exiting...");
+            exit(1);
+        }
+
+        for (int j = 0; j < 120; j++) { // fill array with items
+            array[j] = j;
+        }
+
+        free(array);
+    }
+
+    // ============================================================================================================
+    // ============================================================================================================
+    // ============================================================================================================
+    // ============================================================================================================
+
+
+    // Test 5: Create a 10x12 2D Array. Fill it up with random integers.
+    //         Repeat this 50 times.
+    // ============================================================================================================
+
+
+    for (int x = 0; x < 50; x++) {
+        int **array = (int **) malloc(10 * sizeof(int *)); // Allocate rows of the array.
+
+        for (int y = 0; y < 10; y++) {
+            array[y] = (int *) malloc(12 * sizeof(int)); // Allocate columns of the array.
+        }
+
+        for (int r = 0; r < 10; r++) {
+            for (int c = 0; c < 12; c++) {
+                array[r][c] = rand() % RAND_MAX; // Fill it up with random integers.
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            free(array[i]); // Free everything.
+        }
+
+        free(array); // Free array.
+    } // Repeat this 50 times.
+    return 0;
 }
